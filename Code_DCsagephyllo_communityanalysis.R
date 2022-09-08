8376513ea7f648e40bd26ec36964fb29b1f73128
JacobHeil <jacobheil@boisestate.edu>
2022-09-08 11:45:39 -0600

    uploading R script for community analysis

---
+++ Code_DCsagephyllo_communityanalysis.R
@@ -0,0 +1,5 @@
+#Dry Creek sagebrush phyllosphere project
+#Analysis of the microbial communities on sagebrush leaves
+#Authors: Jacob Heil, Leonora Bittleston
+
+setwd("~/Sagebrush_Results/Ch1/Code/DC_Sagebrush")
