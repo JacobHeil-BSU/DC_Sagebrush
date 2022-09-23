35e5ffe4d5f193bee199f180a5823324a3690d41
JacobHeil <jacobheil@boisestate.edu>
2022-09-22 21:09:32 -0600

    progress

--- Code_DCsagephyllo_communityanalysis.R
+++ Code_DCsagephyllo_communityanalysis.R
@@ -28,7 +28,8 @@ data_ASV <- subset(data_ASV, row.names(data_ASV) %in% row.names(metadata_plant))
 
 ##Naming conventions
 #Assign new names to ASVs
-data_ASV <- data_ASV[,reorder(colnames(data_ASV), order(colnames(data_ASV)))] #put ASVs in numeric order
+data_ASV <- data_ASV[,reorder(colnames(data_ASV), order(colnames(data_ASV)))] 
+#put ASVs in numeric order
 ITS_names <- paste("ITS",sprintf('%0.4d', 1:5473), sep = "") #generate new ASV names
 namingconv_ITS <- data.frame("Original" = colnames(data_ASV), "New" = ITS_names) 
 colnames(data_ASV) <- ITS_names
@@ -46,7 +47,7 @@ data_ASV.r <- data.frame(rrarefy(data_ASV, sample = 4457))
 #Remove any ASVs below desired threshold (default set at 0)
 data_ASV.r <- data_ASV.r[,colSums(data_ASV.r) > 0]
 
-z##Alpha Diversity
+##Alpha Diversity
 #Calculate alpha diversity, add to metadata
 metadata_plant$richness.ITS <- rowSums(data_ASV.r > 0) #richness
 metadata_plant$abundance.ITS <- rowSums(data_ASV.r) #abundance
@@ -63,6 +64,10 @@ boxplot(richness.ITS ~ Date, data = metadata_plant)
 richdate_glm <- glm(richness.ITS ~ Date, data = metadata_plant)
 summary(richdate_glm)
 
+boxplot(richness.ITS ~ Season, data = metadata_plant)
+richseason_glm <- glm(richness.ITS ~ Season, data = metadata_plant)
+summary(richseason_glm)
+
 boxplot(richness.ITS ~ Plant, data = metadata_plant)
 richplant_glm <- glm(richness.ITS ~ Plant, data = metadata_plant)
 summary(richplant_glm) #plant not significant
@@ -75,6 +80,10 @@ plot(allEffects(Richness_glm))
 boxplot(shannon.ITS ~ Date, data = metadata_plant)
 divdate_glm <- glm(shannon.ITS ~ Date, data = metadata_plant)
 summary(divdate_glm)
+
+boxplot(shannon.ITS ~ Season, data = metadata_plant)
+divseason_glm <- glm(shannon.ITS ~ Season, data = metadata_plant)
+summary(divseason_glm)
 
 boxplot(shannon.ITS ~ Plant, data = metadata_plant)
 divplant_glm <- glm(shannon.ITS ~ Plant, data = metadata_plant)
